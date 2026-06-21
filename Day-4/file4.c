d1de9f39df6ca10736ba49128c970895e813a9ca
Nivedita-31 <guptanivi127@gmail.com>
2026-06-21 08:16:08 +0530

    Day-4 assignment is uploaded

---
+++ Day-4/file4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, num, temp, remainder, result;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    printf("Armstrong numbers between %d and %d are:\n", start, end);
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+        result = 0;
+
+        while (temp != 0) {
+            remainder = temp % 10;
+            result += remainder * remainder * remainder;
+            temp /= 10;
+        }
+
+        if (result == num)
+            printf("%d ", num);
+    }
+
+    return 0;
+}
